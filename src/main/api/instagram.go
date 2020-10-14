6bca1eea16d21f61e900255bf97c2e3b67cdae00
Mike <ht5603@yahoo.com.tw>
2020-10-14 23:59:25 +0800

    修正正確的appID

--- src/main/api/instagram.go
+++ src/main/api/instagram.go
@@ -7,8 +7,8 @@ import (
 func GetAccessTokenByCode(code string) { 
 	url := "https://api.instagram.com/oauth/access_token"
 	urlParams := make(map[string]string)
-	urlParams["cliet_id"] = "809042689924382"
-	urlParams["client_secret"] = "00a814d0e83caadde969132ddf703d9b"
+	urlParams["cliet_id"] = "776181923221122"
+	urlParams["client_secret"] = "ff94d60aaf19a49c2b90177fa8a4b639"
 	urlParams["grant_type"] = "authorization_code"
 	urlParams["redirect_uri"] = "https://35.194.194.103/ig/accessToken"
 	urlParams["code"] = code
